559e8cd92272f23c56ffb3a43eb66fe6a9b8b009
victorwkb <victorwkb@gmail.com>
2024-11-10 21:39:45 +1100

    consolidate configs

---
+++ nix/home/tmux.nix
@@ -0,0 +1,90 @@
+{ pkgs, ... }:
+{
+  enable = true;
+  baseIndex = 1;
+  disableConfirmationPrompt = true;
+  keyMode = "vi";
+  newSession = true;
+  secureSocket = true;
+  shell = "${pkgs.zsh}/bin/zsh";
+  shortcut = "a";
+  escapeTime = 0;
+
+  plugins = with pkgs.tmuxPlugins; [
+    {
+      plugin = catppuccin;
+      extraConfig = ''
+        # Catppuccin theme
+        set -g @catppuccin_flavor 'macchiato'
+        set -g @catppuccin_window_status_style "rounded"
+        set -g @catppuccin_window_left_separator ""
+        set -g @catppuccin_window_right_separator " "
+        set -g @catppuccin_window_middle_separator " █"
+        set -g @catppuccin_window_number_position "right"
+        set -g @catppuccin_window_number_color "number"
+        set -g @catppuccin_window_text_color "#W"
+        set -g @catppuccin_window_current_number_color "number"
+        set -g @catppuccin_window_current_text_color "#W#{?window_zoomed_flag,(),}"
+        set -g @catppuccin_status_modules_right "directory date_time"
+        set -g @catppuccin_status_modules_left "session"
+        set -g @catppuccin_status_left_separator  " "
+        set -g @catppuccin_status_right_separator " "
+        set -g @catppuccin_status_right_separator_inverse "no"
+        set -g @catppuccin_status_fill "icon"
+        set -g @catppuccin_status_connect_separator "no"
+        set -g @catppuccin_directory_text "#{b:pane_current_path}"
+        set -g @catppuccin_date_time_text "%H:%M"
+        set -g @catppuccin_status_background "default"
+      '';
+    }
+    {
+      plugin = continuum;
+      extraConfig = "set -g @continuum-restore 'on'";
+
+    }
+    {
+      plugin = resurrect;
+      extraConfig = "set -g @resurrect-capture-pane-contents 'on'";
+    }
+    sensible
+    vim-tmux-navigator
+    yank
+  ];
+
+  extraConfig = ''
+    # fast reload
+    unbind r
+    bind r source-file $HOME/.config/tmux/tmux.conf
+
+    # window splitting
+    unbind %
+    bind | split-window -h
+    unbind '"'
+    bind - split-window -v
+
+    set -g default-terminal "xterm-256color"
+    set -g terminal-overrides ",xterm-256color:RGB"
+    set -g mouse on
+
+    set -g detach-on-destroy off
+    set-option -g focus-events on
+    bind -T copy-mode-vi v send-keys -X begin-selection
+    bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
+    bind -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "pbcopy | tmux load-buffer - && tmux paste-buffer"
+
+    # vim-like pane switching
+    bind -r ^ last-window
+    bind -r h select-pane -L
+    bind -r j select-pane -D
+    bind -r k select-pane -U
+    bind -r l select-pane -R
+    bind -r m resize-pane -Z
+
+    # tpm plugin
+    # set -g @plugin 'tmux-plugins/tpm'
+
+    # tmux sessionizer
+    bind -n C-f run-shell "tmux neww ~/.local/scripts/tmux-sessionizer"
+    bind -r f run-shell "tmux neww ~/.local/scripts/tmux-sessionizer"
+  '';
+}
--- nix/nix-darwin/home.nix
+++ nix/nix-darwin/home.nix
@@ -18,12 +18,9 @@ in
   xdg.configFile.nvim.source = mkOutOfStoreSymlink "/Users/victorwkb/dotfiles/nvim";
 
   home.file = {
-    ".zshrc".source = ~/dotfiles/zsh/.zshrc;
-    ".zsh_profile".source = ~/dotfiles/zsh/.zsh_profile;
-    ".tmux.conf".source = ~/dotfiles/tmux/.tmux.conf;
-    ".config/aerospace".source = ~/dotfiles/aerospace;
-    ".config/personal".source = ~/dotfiles/personal;
-    ".config/starship.toml".source = ~/dotfiles/starship/starship.toml;
+    ".zshrc".source = ../../zsh/.zshrc;
+    ".config/aerospace".source = ../../aerospace/aerospace.toml;
+    ".config/starship.toml".source = ../../starship/starship.toml;
   };
 
   home.sessionPath = [
@@ -35,7 +32,7 @@ in
     git = import ../home/git.nix { inherit pkgs; };
     kitty = import ../home/kitty.nix { inherit pkgs; };
     starship = import ../home/starship.nix { inherit pkgs; };
-    # tmux = import ../home/tmux.nix { inherit pkgs; };
+    tmux = import ../home/tmux.nix { inherit pkgs; };
     zoxide = import ../home/zoxide.nix { inherit pkgs; };
     zsh = import ../home/zsh.nix { inherit pkgs; };
   };
