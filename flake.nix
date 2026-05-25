7b868c0a9406ddd5e4c3785c8f55139b1ae12190
victorwkb <victorwkb@gmail.com>
2026-05-25 20:15:35 +1000

    feat: rewire flake.nix to import from hosts/

--- flake.nix
+++ flake.nix
@@ -49,91 +49,14 @@
     {
       darwinConfigurations."Victors-MacBook-Pro" = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
+        specialArgs = { inherit inputs; };
         modules = [
-          (
-            { pkgs, ... }:
-            {
-              nixpkgs.config.allowUnfree = true;
-
-              environment.systemPackages = [
-                pkgs.awscli
-                pkgs.google-cloud-sdk
-                pkgs.bat
-                pkgs.eza
-                pkgs.fastfetch
-                pkgs.nerdfetch
-                pkgs.cbonsai
-                pkgs.cmatrix
-                pkgs.htop
-                pkgs.pipes
-                pkgs.fd
-                pkgs.fzf
-                pkgs.git
-                pkgs.lazygit
-                pkgs.neovim
-                pkgs.nodejs_22
-                pkgs.carapace
-                pkgs.nushell
-                pkgs.nu_scripts
-                pkgs.ripgrep
-                pkgs.vim
-                pkgs.wget
-                pkgs.nil
-                pkgs.nixfmt
-                pkgs.ghostty-bin
-                pkgs.claude-code
-              ];
-
-              users.users.victorwkb = {
-                name = "victorwkb";
-                home = "/Users/victorwkb";
-              };
-
-              system.primaryUser = "victorwkb";
-
-              homebrew = {
-                enable = true;
-                brews = [ "mas" ];
-                casks = [
-                  "aerospace"
-                  "jordanbaird-ice"
-                  "font-sf-mono"
-                ];
-                masApps = { };
-                taps = [ "nikitabobko/tap" ];
-                onActivation.cleanup = "zap";
-                onActivation.autoUpdate = true;
-                onActivation.upgrade = true;
-              };
-
-              nix.settings.experimental-features = "nix-command flakes";
-              programs.zsh.enable = true;
-              system.configurationRevision = self.rev or self.dirtyRev or null;
-              system.stateVersion = 5;
-
-              system.defaults = {
-                dock.autohide = true;
-                dock.autohide-delay = 0.0;
-                dock.mru-spaces = false;
-                finder.AppleShowAllExtensions = true;
-                finder.FXPreferredViewStyle = "clmv";
-                screencapture.location = "~/Pictures/screenshots";
-                screensaver.askForPasswordDelay = 10;
-                NSGlobalDomain.InitialKeyRepeat = 10;
-                NSGlobalDomain.KeyRepeat = 3;
-              };
-
-              nixpkgs.hostPlatform = "aarch64-darwin";
-              nixpkgs.overlays =
-                (import ./overlays/default.nix { inherit inputs; })
-                ++ [ claude-code.overlays.default ];
-            }
-          )
+          ./hosts/darwin/default.nix
           home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.victorwkb = import ./nix/nix-darwin/home.nix;
+            home-manager.users.victorwkb = import ./hosts/darwin/home.nix;
             home-manager.backupFileExtension = "backup";
           }
           nix-homebrew.darwinModules.nix-homebrew
@@ -154,68 +77,9 @@
 
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
-          (
-            { pkgs, ... }:
-            {
-              nixpkgs.config.allowUnfree = true;
-
-              environment.systemPackages = [
-                pkgs.bat
-                pkgs.eza
-                pkgs.fastfetch
-                pkgs.nerdfetch
-                pkgs.cbonsai
-                pkgs.cmatrix
-                pkgs.htop
-                pkgs.pipes
-                pkgs.fd
-                pkgs.fzf
-                pkgs.git
-                pkgs.lazygit
-                pkgs.neovim
-                pkgs.nodejs_22
-                pkgs.carapace
-                pkgs.nushell
-                pkgs.nu_scripts
-                pkgs.ripgrep
-                pkgs.vim
-                pkgs.tmux
-                pkgs.wget
-                pkgs.nil
-                pkgs.nixfmt-rfc-style
-                pkgs.direnv
-                pkgs.nix-ld-rs
-                pkgs.gcc
-                pkgs.gnumake
-                pkgs.cmake
-                pkgs.clang
-                pkgs.uv
-              ];
-
-              environment.sessionVariables = {
-                DISPLAY = ":0";
-                WAYLAND_DISPLAY = "wayland-0";
-                XDG_RUNTIME_DIR = "/mnt/wslg/runtime-dir";
-              };
-
-              programs.nix-ld.enable = true;
-              programs.zsh.enable = true;
-              services.xserver.enable = true;
-
-              users.users.vicwkb = {
-                isNormalUser = true;
-                group = "vicwkb";
-                extraGroups = [ "wheel" ];
-                password = "";
-              };
-              users.groups.vicwkb = { };
-
-              nix.settings.experimental-features = "nix-command flakes";
-
-              nixpkgs.overlays = import ./overlays/default.nix { inherit inputs; };
-            }
-          )
+          ./hosts/nixos-wsl/default.nix
           nixos-wsl.nixosModules.default
           {
             system.stateVersion = "25.05";
@@ -226,7 +90,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.vicwkb = import ./nix/nixos-wsl/home.nix;
+            home-manager.users.vicwkb = import ./hosts/nixos-wsl/home.nix;
           }
         ];
       };
